68d3b282e38c21ee116c2163f605023fc48fde4c
Rob Reid <15robreid@gmail.com>
2025-02-17 15:07:51 -0500

    Checks
    Reformat scripts - Upper case keywords.
    Added check for Unadvised Data Type.
    Check for Clustered Index - Only check tables with a primary key already.

--- DatabaseChecks.sql
+++ DatabaseChecks.sql
@@ -1,184 +1,203 @@
 
 -- Return potential issues with a database
 
-drop table if exists #Issues;
+DROP TABLE IF EXISTS #Issues;
 
-create table #Issues(
-SchemaName varchar(20) not null,
-ObjectName varchar(50) not null,
-ObjectType varchar(50) not null,
-Issue varchar(50) not null
+CREATE TABLE #Issues(
+	SchemaName VARCHAR(128) not null,
+	ObjectName VARCHAR(128) not null,
+	ObjectType VARCHAR(50) not null,
+	Issue VARCHAR(50) not null
 );
 
 -- Check all tables have a primary key
 
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name] as SchemaName, t.[Name] as TableName, 'Table' as ObjectType, 'No Primary Key' as Issue 
-from sys.tables as t
-join sys.schemas as s
-	on s.schema_id = t.schema_id
-left join sys.key_constraints as k
-	on k.parent_object_id = t.Object_Id
-	and k.[Type] = 'PK'
-where k.object_id is null
-	and t.is_ms_shipped = 0;
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name] as SchemaName, t.[Name] as TableName, 'Table' as ObjectType, 'No Primary Key' as Issue 
+FROM sys.tables as t
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+LEFT JOIN sys.key_constraints as k
+	ON k.parent_object_id = t.Object_Id
+	AND k.[Type] = 'PK'
+WHERE k.object_id is null
+	AND t.is_ms_shipped = 0;
 
 -- Space in column names
 
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name] as SchemaName, t.[Name] + '.' +  c.[Name] as ColumnName, 'Column' as ObjectType, 'Space In Column Name' as Issue
-from sys.columns as c
-join sys.tables as t
-	on t.object_id = c.object_id
-join sys.schemas as s
-	on s.schema_id = t.schema_id
-where c.[Name] like '% %'
-	and t.is_ms_shipped = 0;
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name] as SchemaName, t.[Name] + '.' +  c.[Name] as ColumnName, 'Column' as ObjectType, 'Space In Column Name' as Issue
+FROM sys.columns as c
+JOIN sys.tables as t
+	ON t.object_id = c.object_id
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+WHERE c.[Name] like '% %'
+	AND t.is_ms_shipped = 0;
 
 -- Numbers in column names
 
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name] as SchemaName, t.[Name] + '.' +  c.[Name] as ColumnName, 'Column' as ObjectType, 'Number In Column Name' as Issue
-from sys.columns as c
-join sys.tables as t
-	on t.object_id = c.object_id
-join sys.schemas as s
-	on s.schema_id = t.schema_id
-where c.[Name] like '%[0-9]%'
-	and t.is_ms_shipped = 0;
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name] as SchemaName, t.[Name] + '.' +  c.[Name] as ColumnName, 'Column' as ObjectType, 'Number In Column Name' as Issue
+FROM sys.columns as c
+JOIN sys.tables as t
+	ON t.object_id = c.object_id
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+WHERE c.[Name] like '%[0-9]%'
+	AND t.is_ms_shipped = 0;
 
 -- Space in object name
 
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name] as SchemaName, o.[Name] as ObjectName, o.type_desc as ObjectType, 'Space In Object Name' as Issue
-from sys.objects as o
-join sys.schemas as s
-	on s.schema_id = o.schema_id
-where o.[Name] like '% %'
-	and o.is_ms_shipped = 0;
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name] as SchemaName, o.[Name] as ObjectName, o.type_desc as ObjectType, 'Space In Object Name' as Issue
+FROM sys.objects as o
+JOIN sys.schemas as s
+	ON s.schema_id = o.schema_id
+WHERE o.[Name] like '% %'
+	AND o.is_ms_shipped = 0;
 
 -- Number in object name
 
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name] as SchemaName, o.[Name] as ObjectName, o.type_desc as ObjectType, 'Number In Object Name' as Issue
-from sys.objects as o
-join sys.schemas as s
-	on s.schema_id = o.schema_id
-where o.[Name] like '%[0-9]%'
-	and o.[Type] in ('U', 'V', 'P')
-	and o.is_ms_shipped = 0;
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name] as SchemaName, o.[Name] as ObjectName, o.type_desc as ObjectType, 'Number In Object Name' as Issue
+FROM sys.objects as o
+JOIN sys.schemas as s
+	ON s.schema_id = o.schema_id
+WHERE o.[Name] like '%[0-9]%'
+	AND o.[Type] in ('U', 'V', 'P')
+	AND o.is_ms_shipped = 0;
 
 -- Disabled Foreign Key
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name], t.[Name] + '.' + f.[Name], 'Foreign Key', 'Disabled Foreign Key' 
-from sys.foreign_keys as f
-join sys.tables as t
-	on t.object_id = f.parent_object_id
-join sys.schemas as s
-	on s.schema_id = t.schema_id
-where f.is_ms_shipped = 0
-	and f.is_disabled = 1;
+
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name], t.[Name] + '.' + f.[Name], 'Foreign Key', 'Disabled Foreign Key' 
+FROM sys.foreign_keys as f
+JOIN sys.tables as t
+	ON t.object_id = f.parent_object_id
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+WHERE f.is_ms_shipped = 0
+	AND f.is_disabled = 1;
 
 -- Not Trusted Foreign Key
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name], t.[Name] + '.' + f.[Name], 'Foreign Key', 'Not Trusted Foreign Key' 
-from sys.foreign_keys as f
-join sys.tables as t
-	on t.object_id = f.parent_object_id
-join sys.schemas as s
-	on s.schema_id = t.schema_id
-where f.is_ms_shipped = 0
-	and f.is_not_trusted = 1;
+
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name], t.[Name] + '.' + f.[Name], 'Foreign Key', 'Not Trusted Foreign Key' 
+FROM sys.foreign_keys as f
+JOIN sys.tables as t
+	ON t.object_id = f.parent_object_id
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+WHERE f.is_ms_shipped = 0
+	AND f.is_not_trusted = 1;
 
 -- System Named Foreign Key
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name], t.[Name] + '.' + f.[Name], 'Foreign Key', 'System Named Foreign Key' 
-from sys.foreign_keys as f
-join sys.tables as t
-	on t.object_id = f.parent_object_id
-join sys.schemas as s
-	on s.schema_id = t.schema_id
-where f.is_ms_shipped = 0
-	and f.is_system_named = 1;
+
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name], t.[Name] + '.' + f.[Name], 'Foreign Key', 'System Named Foreign Key' 
+FROM sys.foreign_keys as f
+JOIN sys.tables as t
+	ON t.object_id = f.parent_object_id
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+WHERE f.is_ms_shipped = 0
+	AND f.is_system_named = 1;
 
 -- Disabled Check Constraint
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name], t.[Name] + '.' + f.[Name], 'Check Constraint', 'Disabled Check Constraint' 
-from sys.check_constraints as f
-join sys.tables as t
-	on t.object_id = f.parent_object_id
-join sys.schemas as s
-	on s.schema_id = t.schema_id
-where f.is_ms_shipped = 0
-	and f.is_disabled = 1;
+
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name], t.[Name] + '.' + f.[Name], 'Check Constraint', 'Disabled Check Constraint' 
+FROM sys.check_constraints as f
+JOIN sys.tables as t
+	ON t.object_id = f.parent_object_id
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+WHERE f.is_ms_shipped = 0
+	AND f.is_disabled = 1;
 
 -- Not Trusted Check Constraint
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name], t.[Name] + '.' + f.[Name], 'Check Constraint', 'Not Trusted Check Constraint' 
-from sys.check_constraints as f
-join sys.tables as t
-	on t.object_id = f.parent_object_id
-join sys.schemas as s
-	on s.schema_id = t.schema_id
-where f.is_ms_shipped = 0
-	and f.is_not_trusted = 1;
+
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name], t.[Name] + '.' + f.[Name], 'Check Constraint', 'Not Trusted Check Constraint' 
+FROM sys.check_constraints as f
+JOIN sys.tables as t
+	ON t.object_id = f.parent_object_id
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+WHERE f.is_ms_shipped = 0
+	AND f.is_not_trusted = 1;
 
 -- System Named Check Constraint
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name], t.[Name] + '.' + f.[Name], 'Check Constraint', 'System Named Check Constraint' 
-from sys.check_constraints as f
-join sys.tables as t
-	on t.object_id = f.parent_object_id
-join sys.schemas as s
-	on s.schema_id = t.schema_id
-where f.is_ms_shipped = 0
-	and f.is_system_named = 1;
+
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name], t.[Name] + '.' + f.[Name], 'Check Constraint', 'System Named Check Constraint' 
+FROM sys.check_constraints as f
+JOIN sys.tables as t
+	ON t.object_id = f.parent_object_id
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+WHERE f.is_ms_shipped = 0
+	AND f.is_system_named = 1;
 
 -- System Named Default Constraint
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name], t.[Name] + '.' + f.[Name], 'Default', 'System Named Default' 
-from sys.default_constraints as f
-join sys.tables as t
-	on t.object_id = f.parent_object_id
-join sys.schemas as s
-	on s.schema_id = t.schema_id
-where f.is_ms_shipped = 0
-	and f.is_system_named = 1;
+
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name], t.[Name] + '.' + f.[Name], 'Default', 'System Named Default' 
+FROM sys.default_constraints as f
+JOIN sys.tables as t
+	ON t.object_id = f.parent_object_id
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+WHERE f.is_ms_shipped = 0
+	AND f.is_system_named = 1;
 
 -- Tables with no rows
 
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name], t.[Name], 'Table', 'Empty Table'
-from sys.partitions as p
-join sys.tables as t
-	on t.object_id = p.object_id
-join sys.schemas as s
-	on s.schema_id = t.schema_id
-where t.is_ms_shipped = 0
-	and p.index_id between 0 and 1
-group by s.[Name], t.[Name]
-having sum(p.rows) = 0
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name], t.[Name], 'Table', 'Empty Table'
+FROM sys.partitions as p
+JOIN sys.tables as t
+	ON t.object_id = p.object_id
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+WHERE t.is_ms_shipped = 0
+	AND p.index_id between 0 AND 1
+GROUP BY s.[Name], t.[Name]
+HAVING sum(p.rows) = 0
 
 -- Deprecated data types
 
-insert into #Issues(SchemaName, ObjectName, ObjectType, Issue)
-select s.[Name] as SchemaName, t.[Name] + '.' +  c.[Name] as ColumnName, 'Column' as ObjectType, 
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name] as SchemaName, t.[Name] + '.' +  c.[Name] as ColumnName, 'Column' as ObjectType, 
 	'Deprecated Data Type: ' + ty.[Name] as Issue
-from sys.columns as c
-join sys.tables as t
-	on t.object_id = c.object_id
-join sys.schemas as s
-	on s.schema_id = t.schema_id
-join sys.types as ty
-	on ty.user_type_id = c.user_type_id
-where ty.[Name] in ('image', 'text', 'ntext', 'timestamp')
-	and t.is_ms_shipped = 0;
+FROM sys.columns as c
+JOIN sys.tables as t
+	ON t.object_id = c.object_id
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+JOIN sys.types as ty
+	ON ty.user_type_id = c.user_type_id
+WHERE ty.[Name] in ('image', 'text', 'ntext', 'timestamp')
+	AND t.is_ms_shipped = 0;
+
+INSERT INTO #Issues(SchemaName, ObjectName, ObjectType, Issue)
+SELECT s.[Name] as SchemaName, t.[Name] + '.' +  c.[Name] as ColumnName, 'Column' as ObjectType, 
+	'Unadvised Data Type: ' + ty.[Name] as Issue
+FROM sys.columns as c
+JOIN sys.tables as t
+	ON t.object_id = c.object_id
+JOIN sys.schemas as s
+	ON s.schema_id = t.schema_id
+JOIN sys.types as ty
+	ON ty.user_type_id = c.user_type_id
+WHERE ty.[Name] in ('float', 'sql_variant')
+	AND t.is_ms_shipped = 0;
 	
 -- Return Issues
 
-select SchemaName, ObjectName, ObjectType, Issue
-from #Issues
-order by Issue, SchemaName, ObjectName;
+SELECT SchemaName, ObjectName, ObjectType, Issue
+FROM #Issues
+ORDER BY Issue, SchemaName, ObjectName;
 
-go
-
+GO
--- DatabaseSchemaChecks.sql
+++ DatabaseSchemaChecks.sql
@@ -1,68 +1,88 @@
 
-
 -- Check for disabled or not trusted database objects.
 -- Check for tables without primary keys and/or clustered indexes.
--- For selected tests, GeneratedSql will return SQL to correct the issue.
+-- For Selected tests, GeneratedSql will return SQL to correct the issue.
 
-select object_name(object_id) + '.' + [Name] as ObjectName, 'Foreign Key' as ObjectType, 
+SELECT OBJECT_NAME(OBJECT_ID) + '.' + [Name] as ObjectName, 'Foreign Key' as ObjectType, 
 	'Disabled Foreign Key' as Issue, 
-	'alter table [' + object_name(parent_object_id) + '] with check check constraint [' + [Name] + '];' as GeneratedSql 
-from sys.foreign_keys
-where is_disabled = 1
+	'ALTER TABLE [' + OBJECT_NAME(parent_OBJECT_ID) + '] WITH CHECK CHECK CONSTRAINT [' + [Name] + '];' as GeneratedSql 
+FROM sys.foreign_keys
+WHERE is_disabled = 1
 
-union all
+UNION ALL
 
-select object_name(object_id) + '.' + [Name] as ObjectName, 'Foreign Key' as ObjectType,
+SELECT OBJECT_NAME(OBJECT_ID) + '.' + [Name] as ObjectName, 'Foreign Key' as ObjectType,
 	'Foreign Key Not Trusted' as Issue, 
-	'alter table [' + object_name(parent_object_id) + '] with check check constraint [' + [Name] + '];' as GeneratedSql 
-from sys.foreign_keys
-where is_not_trusted = 1 and is_disabled = 0
+	'ALTER TABLE [' + OBJECT_NAME(parent_OBJECT_ID) + '] WITH CHECK CHECK CONSTRAINT [' + [Name] + '];' as GeneratedSql 
+FROM sys.foreign_keys
+WHERE is_not_trusted = 1 AND is_disabled = 0
 
-union all
+UNION ALL
 
-select object_name(parent_obj) + '.' + [Name] as ObjectName, 'Trigger' as ObjectType, 'Disabled Trigger' as Issue, 
-	'alter table [' + object_name(parent_obj) + '] enable trigger [' + [Name] + '];' as GeneratedSql
-from dbo.sysobjects where XType = 'TR'
+SELECT OBJECT_NAME(parent_obj) + '.' + [Name] as ObjectName, 'Trigger' as ObjectType, 'Disabled Trigger' as Issue, 
+	'ALTER TABLE [' + OBJECT_NAME(parent_obj) + '] ENABLE TRIGGER [' + [Name] + '];' as GeneratedSql
+FROM dbo.sysobjects WHERE XType = 'TR'
 and objectproperty(ID, 'ExecIsTriggerDisabled') = 1
 
-union all
+UNION ALL
 
-select object_name(cc.parent_object_id) + '.' + cc.name as ObjectName, 'Check Constraint' as ObjectType,
+SELECT OBJECT_NAME(cc.parent_OBJECT_ID) + '.' + cc.name as ObjectName, 'Check Constraint' as ObjectType,
 	'Constraint Not Trusted' as Issue, '' as GeneratedSql
-from sys.check_constraints as cc
-where cc.is_not_trusted = 1
+FROM sys.check_constraints as cc
+WHERE cc.is_not_trusted = 1
 
-union all
+UNION ALL
 
-select object_name(cc.parent_object_id) + '.' + cc.name as ObjectName, 'Check Constraint' as ObjectType,
-	'Constraint Disabled' as Issue, 'alter table [' + object_name(cc.parent_object_id) + '] with check check constraint [' + cc.name + '];' as GeneratedSql
-from sys.check_constraints as cc
-where cc.is_disabled = 1
+SELECT OBJECT_NAME(cc.parent_OBJECT_ID) + '.' + cc.name as ObjectName, 'Check Constraint' as ObjectType,
+	'Constraint Disabled' as Issue, 'ALTER TABLE [' + OBJECT_NAME(cc.parent_OBJECT_ID) + '] WITH CHECK CHECK CONSTRAINT [' + cc.name + '];' as GeneratedSql
+FROM sys.check_constraints as cc
+WHERE cc.is_disabled = 1
 
-union all
+UNION ALL
 
-select t.name as TableName, 'Table' as ObjectType, 'No Clustered Index' as Issue, '' as GeneratedSql 
-from sys.tables as t
-left join sys.indexes as i
-	on i.object_id = t.object_id
-	and i.type = 1 -- Clustered
-where i.object_id is null
+-- Only return check for No Clustered Index if table has a primary key
 
-union all
+SELECT CONCAT(SCHEMA_NAME(t.SCHEMA_ID), '.', t.[Name]) as TableName, 'Table' as ObjectType, 'No Clustered Index' as Issue, 
+	'' as GeneratedSql 
+FROM sys.tables as t
+JOIN sys.indexes as pk
+	ON pk.OBJECT_ID = t.OBJECT_ID
+	AND pk.is_primary_key = 1
+LEFT JOIN sys.indexes as i
+	ON i.OBJECT_ID = t.OBJECT_ID
+	AND i.type = 1 -- Clustered
+WHERE i.OBJECT_ID IS NULL
 
-select t.[Name] as ObjectName, 'Table' as ObjectType, 'No Primary Key' as Issue, '' as GeneratedSql 
-from sys.tables as t
-left join sys.key_constraints as c
-	on c.parent_object_id = t.[object_id]
-where c.parent_object_id is null
+UNION ALL
 
-union all
+SELECT CONCAT(SCHEMA_NAME(t.SCHEMA_ID), '.', t.[Name]) as ObjectName, 'Table' as ObjectType, 'No Primary Key' as Issue, 
+	CONCAT('ALTER TABLE [', SCHEMA_NAME(t.SCHEMA_ID), '].[', t.[name], '] ADD CONSTRAINT ',
+	'[PK_', SCHEMA_NAME(t.SCHEMA_ID), t.[Name], '] PRIMARY KEY CLUSTERED (<column>);') as GeneratedSql 
+FROM sys.tables as t
+LEFT JOIN sys.key_constraints as c
+	ON c.parent_OBJECT_ID = t.[OBJECT_ID]
+WHERE c.parent_OBJECT_ID IS NULL
 
-select [Name] as ObjectName, [Type] as ObjectType, 'ANSI NULLS Off' as Issue, '' as GeneratedSql
-from sys.objects 
-where [Type] in ('TR', 'P', 'FN', 'IF', 'TF', 'U')
-and objectproperty (object_id,'IsAnsiNullsOn') = 0
+UNION ALL
 
-order by 1
+SELECT [Name] as ObjectName, [Type] as ObjectType, 'ANSI NULLS Off' as Issue, '' as GeneratedSql
+FROM sys.objects 
+WHERE [Type] IN ('TR', 'P', 'FN', 'IF', 'TF', 'U')
+	AND OBJECTPROPERTY (OBJECT_ID,'IsAnsiNullsOn') = 0
 
-go
+UNION ALL
+
+SELECT s.[Name] AS SchemaName, 'Schema' AS ObjectType, 'Empty Schema' AS Issue,
+	CONCAT('DROP SCHEMA [', s.[name], '];') AS GeneratedSql
+FROM sys.schemas AS s
+JOIN sys.database_principals AS p
+	ON p.principal_id = s.principal_id
+LEFT JOIN sys.objects AS o
+	ON o.schema_id = s.schema_id
+WHERE p.is_fixed_role = 0
+	AND s.[name] NOT IN ('guest', 'INFORMATION_SCHEMA', 'sys', 'dbo')
+	AND o.[name] IS NULL
+
+ORDER BY 1
+
+GO
